7c3a29a728c61d95280825151514b2936b4b38d0
Oner2me <947459097@qq.com>
2018-07-19 11:50:07 +0800

    home页面已写完

--- my-project/src/page/HomePage.vue
+++ my-project/src/page/HomePage.vue
@@ -47,14 +47,14 @@
                         </div>
                     </div>
                     <div class="ns-right">
-                        <div class="ns-top">
+                        <div class="ns-box">
                             <img src="../assets/images/home-ns-2.png" alt="home-ns-2.png">
                             <div class="ns-right-text">
                                 <span>半月板损伤护膝</span>
                                 <span>运动保护 APTONIAK</span>
                             </div>
                         </div>
-                        <div class="ns-bottom">
+                        <div class="ns-box">
                             <img src="../assets/images/home-ns-3.png" alt="home-ns-3.png">
                             <div class="ns-right-text">
                                 <span>TMT护膝运动男跑</span>
@@ -64,18 +64,36 @@
                     </div>
                 </div>
                 <div class="home-more">
-                    MORE
+                    <span>MORE</span>
                 </div>
             </div>
             <div class="home-img">
-                <img src="" alt="">
+                <img src="../assets/images/home-banner-two.png" alt="home-banner-two.png">
             </div>
             <div class="home-hots-shop">
                 <h2>热卖商品</h2>
-                <img src="" alt="">
+                <div class="home-hs-img">
+                    <img src="../assets/images/home-hs.png" alt="home-hs.png">
+                    <div class="home-hs-font">
+                        <span>运动舞蹈护肘护膝小孩儿童女套装</span>
+                        <span>足球跳舞跪地男童女童防摔护膝夏455</span>
+                    </div>
+                </div>
                 <div class="hs">
-                    <div class="hs-left"></div>
-                    <div class="hs-right"></div>
+                    <div class="hs-box">
+                        <img src="../assets/images/home-hs-girl.png" alt="home-hs-girl.png">
+                        <div class="hs-font">
+                            <span>女款运动护</span>
+                            <span>肘护膝小孩儿童</span>
+                        </div>
+                    </div>
+                    <div class="hs-box">
+                        <img src="../assets/images/home-hs-boy.png" alt="home-hs-boy.png">
+                        <div class="hs-font">
+                            <span>男款运动护肘</span>
+                            <span>护膝小孩儿童</span>
+                        </div>
+                    </div>
                 </div>
                 <div class="home-more">
                     <span>MORE</span>
@@ -83,29 +101,71 @@
             </div>
             <div class="home-best-shop">
                 <h2>精品特卖</h2>
-                <div class="bs-cate-top"></div>
+                <div class="bs-cate-top">
+                    <div class="bs-cate">
+                        <img src="../assets/images/home-bs-1.png" alt="home-bs-1.png">
+                        <span>户外打猎...</span>
+                    </div>
+                    <div class="bs-cate">
+                        <img src="../assets/images/home-bs-2.png" alt="home-bs-2.png">
+                        <span>军绿色布...</span>
+                    </div>
+                    <div class="bs-cate">
+                        <img src="../assets/images/home-bs-3.png" alt="home-bs-3.png">
+                        <span>涤纶布枪...</span>
+                    </div>
+                </div>
                 <div class="bs-cate-bottom">
-                    <div class="hs-left"></div>
-                    <div class="hs-right"></div>
+                    <div class="bs-box">
+                        <img src="../assets/images/home-bs-4.png" alt="home-bs-4.png">
+                        <div class="bs-text">
+                            春夏季仿生迷彩打猎...
+                        </div>
+                    </div>
+                    <div class="bs-box">
+                        <img src="../assets/images/home-bs-5.png" alt="home-bs-5.png">
+                        <div class="bs-text">
+                            酋长户外战术迷彩...
+                        </div>
+                    </div>
                 </div>
                 <div class="home-more">
-                    MORE
+                    <span>MORE</span>
                 </div>
             </div>
             <div class="home-recommend">
                 <h2>良心推荐</h2>
                 <div class="rec">
-                    <div class="rec-left"></div>
-                    <div class="rec-right"></div>
-                    <span></span>
+                    <div class="rec-left">
+                        <img src="../assets/images/home-rec-1.png" alt="home-rec-1.png">
+                        <div class="rl-text">
+                            野战背心...
+                        </div>
+                    </div>
+                    <div class="rec-right">
+                        <img src="../assets/images/home-rec-2.png" alt="home-rec-2.png">
+                        <div class="rr-text">
+                            户外装备...
+                        </div>
+                    </div>
                 </div>
                 <div class="rec">
-                    <div class="rec-left"></div>
-                    <div class="rec-right"></div>
-                    <span></span>
+                    <div class="rec-left">
+                        <img src="../assets/images/home-rec-3.png" alt="home-rec-3.png">
+                        <div class="rl-text">
+                            环保塑料...
+                        </div>
+                    </div>
+                    <div class="rec-right">
+                        <img src="../assets/images/home-rec-4.png" alt="home-rec-4.png">
+                        <div class="rr-text">
+                            厂家生产...
+                        </div>
+                    </div>
                 </div>
+
                 <div class="home-more">
-                    MORE
+                    <span>MORE</span>
                 </div>
             </div>
         </section>
